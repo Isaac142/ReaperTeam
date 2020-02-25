649f1d0c21529178655b4eaea97cbeeca3bc39ca
John <donjuanbuizon@gmail.com>
2020-02-25 18:44:38 +1100

    Auto stash before merge of "master" and "origin/master"

---
+++ ReaperJr/Assets/OwnScenes/John/Scripts/ThirdPersonCharacterController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCharacterController : MonoBehaviour
+{
+    Rigidbody controller;
+
+    public float Speed;
+
+    public float velocity = 10f;
+    public bool isGrounded;
+    public float distanceGround = 1.2f;
+
+    public LayerMask groundLayer;
+
+    private void Start()
+    {
+        controller = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+        Grounded();
+    }
+
+    void PlayerMovement()
+    {
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+        Vector3 playerMovement = new Vector3(hor, 0f, ver) * Speed * Time.deltaTime;
+        transform.Translate(playerMovement, Space.Self);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            Jump();
+        }
+
+        void Jump()
+        {
+            controller.AddForce(new Vector3(0, velocity, 0));
+        }
+
+    }
+    void Grounded()
+    {
+        RaycastHit hit;
+        Vector3 dir = new Vector3(0, -1, 0);
+
+        if (Physics.Raycast(transform.position, dir, out hit, distanceGround))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+    }
+}
