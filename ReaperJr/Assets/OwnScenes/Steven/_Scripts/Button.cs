844b75bf137c1adb33405f3acaee11f3f22bfbe1
Steven K <spkranjac@gmail.com>
2020-03-03 18:58:41 +1100

    Created Tutorial level, Camera clamp, moving platforms

---
+++ ReaperJr/Assets/OwnScenes/Steven/_Scripts/Button.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    public GameObject door;
+
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(door);
+            Debug.Log(" Door Destroyed ");
+        }
+    }
+}
---
+++ ReaperJr/Assets/OwnScenes/Steven/_Scripts/SoulCollect.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoulCollect : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+            Debug.Log(" Soul Collected");
+        }
+    }
+}
