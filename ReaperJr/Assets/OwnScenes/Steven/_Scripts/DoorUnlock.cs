45bdbd0ed3c2d291adf789390951ad5f4af2a32a
Steven <spkranjac@gmail.com>
2020-04-20 20:54:29 +1000

    Updated Playground
    - Updated and completed 2nd area for playground
    - Removed front boundary as not able to click through.

--- ReaperJr/Assets/OwnScenes/Steven/_Scripts/DoorUnlock.cs
+++ ReaperJr/Assets/OwnScenes/Steven/_Scripts/DoorUnlock.cs
@@ -5,20 +5,55 @@ using UnityEngine;
 public class DoorUnlock : MonoBehaviour
 {
     public Animator lockedDoor;
+    public bool box1 = false;
+    public bool box2 = false;
+    public bool box3 = false;
 
-     
 
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Box")
         {
-            lockedDoor.SetBool("LockedDoor", true);
+            box1 = true;
+            Debug.Log(" Box 1 Placed");
+        }
 
-            Debug.Log(" Key Set... Now Unlocking ");
+        if (other.gameObject.tag == "Box2")
+        {
+            box2 = true;
+            Debug.Log(" Box 2 Placed");
+        }
+
+        if (other.gameObject.tag == "Box3")
+        {
+             box3 = true;
+             Debug.Log(" Box 3 Placed");
         }
         else
         {
-            Debug.Log(" Key Required ");
+            Debug.Log(" Keys Required ");
         }
+
+        if (box1 == true )
+        {
+            if(box2 == true)
+            {
+                if(box3 == true)
+                {
+                    DoorOpen();
+                }
+            }           
+            
+        }
+                          
+        
+    }
+
+    void DoorOpen()
+    {
+        
+         lockedDoor.SetBool("LockedDoor", true);
+         Debug.Log(" Key Set... Now Unlocking ");
+       
     }
 }
