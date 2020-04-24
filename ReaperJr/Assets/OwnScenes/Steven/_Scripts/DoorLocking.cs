e5392d44fde383b14ed4e6be87c28fbbf3335de4
Steven <spkranjac@gmail.com>
2020-04-24 13:09:19 +1000

    Playground Tweeking
    - Changed Playground to resemble attic concept to get a feel how the level would play
    - Added movable boxes
    - Added Souls to collect
    - Added moving boxes when bigger box is moved by player 
    - Added boxes that fall as player walks up stairs 
    - Added teleport point for player to reach other souls / key that are too high to reach unless you have gone up the stairs
    - Added Fake Soul
    - Added door that locks behind player that will need to be unlocked to exit room
    - Removed trap door as player would be locked out of the room if they feel through back to either the hall way or another room
    - Added Materials to the walls,floors and boxes so it wasn't all white

---
+++ ReaperJr/Assets/OwnScenes/Steven/_Scripts/DoorLocking.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorLocking : MonoBehaviour
+{
+    public GameObject Door;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Door.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Door.gameObject.SetActive(true);
+        }
+    }
+}
