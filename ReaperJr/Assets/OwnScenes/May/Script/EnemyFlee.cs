9512a4e1760de7b46f1804c007e34feb0233b152
MayWu <DWu@myjmc.edu.au>
2020-04-27 02:22:42 +1000

    FleeMechanic
    if player approach, enemy flees

---
+++ ReaperJr/Assets/OwnScenes/May/Script/EnemyFlee.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyFlee : MonoBehaviour
+{
+    public GameObject player;
+
+    private NavMeshAgent agent;
+    public float detectRange = 5f;
+    public List<Transform> fleePoint;
+    public int fleePointIndex;
+
+    private Vector3 runDirection;
+    bool RunDirTest(Vector3 newPos, out Vector3 result)
+    {
+            NavMeshHit hit;
+            if (NavMesh.SamplePosition(newPos, out hit, 1.0f, NavMesh.AllAreas))
+            {
+                result = hit.position;
+                return true;
+            }
+        result = Vector3.zero;
+        return false;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float distance = Vector3.Distance(player.transform.position, transform.position);
+        if(distance <= detectRange)
+        {
+            runDirection = (transform.position - player.transform.position);
+            Vector3 newPosition = transform.position + runDirection;
+            Vector3 newPos;
+
+            if (RunDirTest(newPosition, out newPos))
+                agent.SetDestination(newPosition);
+            else
+                NextFleePoint();
+        }
+    }
+
+    void NextFleePoint()
+    {
+        if (fleePoint.Count > 0)
+        {
+            agent.destination = fleePoint[fleePointIndex].position;
+            fleePointIndex++;
+            fleePointIndex %= fleePoint.Count; //cycling index number.
+        }
+    }
+}
