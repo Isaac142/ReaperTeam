52d3653ee87a461315ae3d6374e198d7bbb67df2
Steven <spkranjac@gmail.com>
2020-05-07 13:43:43 +1000

    2nd Playground
    - Playground based on the mind map
    - Door and wall renderers turned off on trigger enter / exit
    - 2nd level with stairs to create attic room

---
+++ ReaperJr/Assets/OwnScenes/Steven/_Scripts/wallrenderer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wallrenderer : MonoBehaviour
+{
+    public Renderer wall;
+    public Renderer door;
+    // Start is called before the first frame update
+    void Start()
+    {
+        wall.gameObject.GetComponent<Renderer>();
+        door.gameObject.GetComponent<Renderer>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            wall.GetComponent<Renderer>().enabled = false;
+            door.GetComponent<Renderer>().enabled = false;
+            Debug.Log(" Player entered the room");
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            wall.GetComponent<Renderer>().enabled = true;
+            door.GetComponent<Renderer>().enabled = true;
+            Debug.Log(" Player Left the room");
+        }
+
+    }
+}
