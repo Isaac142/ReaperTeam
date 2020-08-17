2ad355263f66f0203c48646e10f7133691ef7769
Steven <spkranjac@gmail.com>
2020-08-17 23:04:36 +1000

    Main Menu Update
    - Music Slider and toggle working as intended ( able to turn off music or lower music volume
    - Sound not updated as we have no sounds on main menu

---
+++ ReaperJr/Assets/OwnScenes/Steven/_Scripts/MusicSlider.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicSlider : MonoBehaviour
+{
+    public AudioSource music;
+    public float musicVolume = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        music.volume = musicVolume;
+    }
+
+    public void volumeUpdate( float volume)
+    {
+        musicVolume = volume;
+    }
+}
